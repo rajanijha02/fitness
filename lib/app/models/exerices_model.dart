33bd6ff6f2c7d84d48a499105fe9f91f4ea0fe95
frenzycoder <=>
2023-04-23 02:38:58 +0530

    Created Exercise , GlobalExercise and Program Model

---
+++ lib/app/models/exerices_model.dart
@@ -0,0 +1,29 @@
+class Exercise {
+  String programId;
+  String name;
+  String description;
+  String id;
+  String image;
+
+  Exercise({
+    required this.programId,
+    required this.name,
+    required this.description,
+    required this.id,
+    required this.image,
+  });
+}
+
+class GlobalExercise {
+  String name;
+  String description;
+  String id;
+  String image;
+
+  GlobalExercise({
+    required this.name,
+    required this.description,
+    required this.id,
+    required this.image,
+  });
+}
---
+++ lib/app/models/program_model.dart
@@ -0,0 +1,7 @@
+class Program {
+  String name;
+  String description;
+  String id;
+
+  Program({required this.name, required this.description, required this.id});
+}
